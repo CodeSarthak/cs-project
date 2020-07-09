037f304902b2b920e03ead5ff3090663e09c995a
akshat trehan <akshat.trehan@yahoo.com>
2020-07-09 17:41:26 +0530

    Update main project gui.py

--- main project gui.py
+++ main project gui.py
@@ -1,6 +1,7 @@
 from tkinter.ttk import *
 from tkinter import *
 window=Tk()
+window.geometry('750x600')
 admno=Label(text='Enter your admission no.',fg='white',bg='red')
 admno.grid(column=0,row=0)
 entry1=Entry(fg='white',bg='grey',width=20)
@@ -20,22 +21,15 @@ branch.insert(3,'AISSAKET')
 branch.grid(column=0,row=6)
 def func():
     from firebase import firebase
-    import http 
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
+
     firebase = firebase.FirebaseApplication("https://python-interface-55bc7.firebaseio.com/", None)
-    result = firebase.get("python-interface-55bc7/test", '')
-    dict1 = {'Hi':1, "Hello":2}
-    akshat = str(list(result.values())[1]["Number"])
-    chrome_options = Options()  
-#chrome_options.add_argument("--headless") 
-    url = "https://web.whatsapp.com/send?phone=+91"+akshat
-
-
-    driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"),  options =  chrome_options)  
-    driver.maximize_window()
-    driver.get(url)
-    username_textbox = driver.find_element_by_id("_3FRCZ copyable-text selectable-text")
+    data = {    
+    'Name':'Helldcibdibo',
+    'Email' :'test@gmail.com',
+    'Number': 9599499407
+     }
+    result = firebase.post("python-interface-55bc7/test", data)
+    print(result)
 
 
 bt=Button(text='submit',command=func)
