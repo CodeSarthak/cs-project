73801f0e36acfb9f2e9f83cd53f9317dca91976a
akshat trehan <akshat.trehan@yahoo.com>
2020-07-21 21:09:21 +0530

    Update telegram prog.py

--- telegram prog.py
+++ telegram prog.py
@@ -1,13 +1,29 @@
-import telebot
 import time
-bot_token='1392448049:AAHTH2h74FL8xrVsOPmwoVduifTiRGBylMo'
-bot = telebot.TeleBot(token=bot_token)
-@bot.message_handler(commands=['start'])
+import telebot
+
+TOKEN = "1392448049:AAHedLc3i0cqF61kubWlOiloawlt4JHY80s"
+bot = telebot.TeleBot(token=TOKEN)
+
+
+
+@bot.message_handler(commands=['start']) # welcome message handler
 def send_welcome(message):
-    bot.reply_to(message,'Welcome , enter your admission no. and passwd')
-@bot.message_handler(commands=['help'])
+    bot.reply_to(message, 'Hey, there! Type "/help" to get help on how to get started! ')
+
+@bot.message_handler(commands=['help']) # help message handler
 def send_welcome(message):
-    bot.reply_to(message,'To start your bot enter your admission no. and passwd')
-    
+    bot.reply_to(message, 'Type in your admission number and password in this format to get your homework : [admission number] [password]. For example, 1234 password')
+while True:
+    try:
+        bot.polling(none_stop=True)
+        # ConnectionError and ReadTimeout because of possible timout of the requests library
+        # maybe there are others, therefore Exception
+    except Exception:
+        time.sleep(5)
+
+
+
+
+
 
 
