2d2b1f5229eb5f6493b5459d25d1fe688deb467e
akshat trehan <akshat.trehan@yahoo.com>
2020-09-23 11:57:58 +0530

    Update main project gui.py

--- main project gui.py
+++ main project gui.py
@@ -35,19 +35,16 @@ def func():
     # driver.maximize_window()
     # driver.get(url)
     # username_textbox = driver.find_element_by_id("_3FRCZ copyable-text selectable-text")
-
-
 bt=Button(text='submit',command=func())
-
-
-    firebase = firebase.FirebaseApplication("https://python-interface-55bc7.firebaseio.com/", None)
-    data = {    
-    'Name':'Helldcibdibo',
+firebase = firebase.FirebaseApplication("https://python-interface-55bc7.firebaseio.com/", None)
+data = {'Name':'Helldcibdibo',
     'Email' :'test@gmail.com',
     'Number': 9599499407
      }
-    result = firebase.post("python-interface-55bc7/test", data)
-    print(result)
+result = firebase.post("python-interface-55bc7/test", data)
+print(result)
+
+
 
 
 bt=Button(text='submit',command=func)
